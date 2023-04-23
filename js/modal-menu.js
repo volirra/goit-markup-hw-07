4dd831e622bcc89b6f74c03dbc36247423dc53ed
volirra <127318805+volirra@users.noreply.github.com>
2023-04-23 19:24:20 +0300

    mod-7

---
+++ js/modal-menu.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[js-open-menu]"),
+    closeModalBtn: document.querySelector("[js-close-menu]"),
+    modal: document.querySelector("[js-menu-container]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-open");
+  }
+})();
